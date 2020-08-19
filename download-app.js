545f4b01db5fa689a2d038af710a08a13c5e5159
wdlairpgv62 <53283430+wdlairpgv62@users.noreply.github.com>
2020-08-19 12:31:00 +0800

    Add files via upload

---
+++ download-app.js
@@ -0,0 +1,79 @@
+//betsm
+function getSoftWare_betsm() {
+		window.open("https://share.weiyun.com/52aOynG");
+}
+function getSoftAndroid_betsm() {
+		window.open("https://cdn.jsdelivr.net/gh/wdlairpgv62/js/version/BetSPSSModeler.apk");
+}
+function getSoftIOS_betsm() {
+		window.open("https://swdzsw.cn/4REUTl");
+}
+function getSoftWareRoll_betsm() {
+	window.open("https://share.weiyun.com/q8i0qVxy");
+}
+function getSoftAndroidRoll_betsm() {
+	window.open("https://cdn.jsdelivr.net/gh/wdlairpgv62/js/version/BETSM走地版.apk");
+}
+function getSoftIOSRoll_betsm() {
+	window.open("https://swdzsw.cn/app.php/NjQyMA==");
+}
+//roll
+function getSoftWare_roll() {
+	window.open("https://share.weiyun.com/5u7bMb5");
+}
+function getSoftAndroid_roll() {
+	window.open("https://cdn.jsdelivr.net/gh/wdlairpgv62/janq/roll/common/js/RollingPrediction.apk");
+}
+function getSoftIOS_roll() {
+	window.open("https://swdzsw.cn/app.php/NDk4MQ==");
+}
+//ana
+function getSoftWare_ana() {
+	window.open("https://share.weiyun.com/55FuKS9");
+}
+function getSoftAndroid_ana() {
+	window.open("https://share.weiyun.com/5MDi0cv");
+}
+function getSoftIOS_ana() {
+	window.open("https://swdzsw.cn/app.php/NDk3Nw==");
+}
+//be
+function getSoftWare_be() {
+	window.open("https://share.weiyun.com/5EDa4Tv");
+}
+function getSoftAndroid_be() {
+	window.open("https://share.weiyun.com/5Fe8CPk");
+}
+function getSoftIOS_be() {
+	window.open("https://swdzsw.cn/app.php/NDk3NQ==");
+}
+//future
+function getSoftWare_future() {
+	window.open("https://share.weiyun.com/5saVXvA");
+}
+function getSoftAndroid_future() {
+	window.open("https://share.weiyun.com/5ORM07P");
+}
+function getSoftIOS_future() {
+	window.open("https://swdzsw.cn/ZwXhzc");
+}
+//porto
+function getSoftWare_porto() {
+	window.open("https://share.weiyun.com/3akSWqF1");
+}
+function getSoftAndroid_porto() {
+	window.open("https://share.weiyun.com/vclR5Bh4");
+}
+function getSoftIOS_porto() {
+	window.open("https://swdzsw.cn/app.php/NTg0Mw==");
+}
+//keen
+function getSoftWare_keen() {
+	window.open("https://share.weiyun.com/YgmYSaMz");
+}
+function getSoftAndroid_keen() {
+	window.open("https://share.weiyun.com/8aHppAPx");
+}
+function getSoftIOS_keen() {
+	window.open("https://swdzsw.cn/app.php/ODY2NA==");
+}
